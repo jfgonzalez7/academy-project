588e61b054f5d6e5759016f20de4dc17caa484d7
dangarita <dangarita@sofi.org>
2020-08-10 12:09:49 -0500

    Client list service

--- src/app/clients/client-list/client-list.service.ts
+++ src/app/clients/client-list/client-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ClientesList } from './ClientesList.model';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {  HttpClient } from '@angular/common/http';
 
 
 
